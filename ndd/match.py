3b93ea29ee6103d48dc1a988725ceca4370fbb20
bkj <ben@gophronesis.com>
2016-07-20 19:18:03 -0400

    adding to_dict method

--- ndd/match.py
+++ ndd/match.py
@@ -4,3 +4,10 @@ class match:
         self.matches = set(matches)
         self.has_match = len(matches) > 0
         self.method = method
+    
+    def to_dict(self):
+        return {
+            "matches" : self.matches,
+            "has_match" : self.has_match,
+            "method" : self.method
+        }
