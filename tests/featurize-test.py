5ba3e82a277f5bb159c9ff6c63c2a24693f07074
Ben Johnson <bkj@users.noreply.github.com>
2016-08-12 14:51:36 -0400

    Update featurize-test.py

--- tests/featurize-test.py
+++ tests/featurize-test.py
@@ -18,6 +18,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--inpath', type=str, default='./data/*jpg')
     parser.add_argument('--outpath', type=str, default='./db.h5')
+    parser.add_argument('--verbose', action="store_true")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -28,6 +29,9 @@ if __name__ == "__main__":
     db = h5py.File(args.outpath)
 
     for file in glob(args.inpath):
+        if verbose:
+            print >> sys.stderr, file
+        
         img = ndd.utils.load_img(file)
         hashes = nh.hash_function(img)
         for method,hash_ in zip(nh.methods, hashes):
