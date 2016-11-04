8144c434150011e316d194560a10e3cb89c3a9c7
bkj <ben@gophronesis.com>
2016-11-04 16:50:08 -0400

    allow convnet to fail

--- ndd/__init__.py
+++ ndd/__init__.py
@@ -1,10 +1,12 @@
 from hashlibDB import Hashlib
 from imagehashDB import Imagehash
 from imagehashRedisDB import ImagehashRedis
+
 try:
     from convnetDB import ConvNet
 except:
     pass
+
 from multihash import Multihash
 from match import match
 import utils
